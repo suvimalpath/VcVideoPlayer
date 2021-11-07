b03541004324139493dd89fb43a0182ed1890ff1
PATRICIA <87294373+TEAM-PATRICIA@users.noreply.github.com>
2021-11-07 10:25:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,27 @@
 import os
-
 from os import getenv
 from dotenv import load_dotenv
 
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
 load_dotenv()
 admins = {}
-ADMIN = int(os.getenv('ADMIN',1956381927))
-CHANNEL = int(os.getenv('CHANNEL',12345))
-API_ID = int(os.getenv("API_ID", "6"))
-API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-BOT_USERNAME = os.getenv("BOT_USERNAME", "VcVideoRobot")
-ASSISTANT_NAME = os.getenv("ASSISTANT_NAME", "Vc Video Player")
-SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "DeeCodeSupport")
-UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", "DeeCodeBots")
-SOURCE_CODE = os.getenv("SOURCE_CODE", "github.com/Sammy-XD/VcVideoPlayer")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-SESSION_NAME = os.getenv("SESSION_NAME")
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME", "Video Stream")
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+OWNER_NAME = getenv("OWNER_NAME", "piroXpower")
+ALIVE_NAME = getenv("ALIVE_NAME", "VideoPlayer")
+BOT_USERNAME = getenv("BOT_USERNAME", "VcVideoRoBot")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "Decode_Assistant")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "DeCodeSupport")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "DeecodeBots")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+ALIVE_IMG = getenv("ALIVE_IMG", "https://te.legra.ph/file/3230fb4f2943318939118.jpg")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "190"))
+IMG_1 = getenv("IMG_1", "https://telegra.ph/file/d6f92c979ad96b2031cba.png")
+IMG_2 = getenv("IMG_2", "https://telegra.ph/file/6213d2673486beca02967.png")
+IMG_3 = getenv("IMG_3", "https://telegra.ph/file/f02efde766160d3ff52d6.png")
