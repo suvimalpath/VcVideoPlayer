223d63382603d5e91989840422a98bc0475359c1
PATRICIA <87294373+TEAM-PATRICIA@users.noreply.github.com>
2021-11-07 10:33:09 +0530

    Update videoplayer.py

--- player/videoplayer.py
+++ player/videoplayer.py
@@ -52,7 +52,7 @@ async def ytdl(link):
         return 0, stderr.decode()
 
 
-@Client.on_message(command(["vplay", f"vplay@{BOT_USERNAME}"]) & other_filters)
+@Client.on_message(command(["stream", f"stream@{BOT_USERNAME}"]) & other_filters)
 async def vplay(c: Client, m: Message):
     replied = m.reply_to_message
     chat_id = m.chat.id
